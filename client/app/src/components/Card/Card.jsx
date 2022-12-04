d71ab38b644349e66a9dc8dfeafb442b39598e3a
innazozulia <innazozulia@outlook.com>
2022-12-04 19:41:09 +0200

    added Strapi content for Featured Component and tune filter

--- client/app/src/components/Card/Card.jsx
+++ client/app/src/components/Card/Card.jsx
@@ -3,28 +3,35 @@ import { Link } from "react-router-dom";
 import "./Card.scss";
 
 const Card = ({ item }) => {
+  console.log(process.env.REACT_APP_UPLOAD_URL);
   return (
     <Link
       className="link"
       to={`/product/${item.id}`}>
       <div className="card">
         <div className="image">
-          {item.isNew && <span>New Collection</span>}
+          {item?.attributes.isNew && <span>New Collection</span>}
           <img
             className="main-img"
-            src={item.img}
+            src={
+              "http://localhost:1337" +
+              item.attributes?.img?.data?.attributes?.url
+            }
             alt="product"
           />
           <img
             className="second-img"
-            src={item.img2}
+            src={
+              "http://localhost:1337" +
+              item.attributes?.img2?.data?.attributes?.url
+            }
             alt="product"
           />
         </div>
-        <h2>{item.title}</h2>
+        <h2>{item?.attributes.title}</h2>
         <div className="prices">
-          <h3>{item.oldPrice} $</h3>
-          <h3>{item.price} $</h3>
+          <h3>{item?.attributes.oldPrice || item?.attributes.price + 20} $</h3>
+          <h3>{item?.attributes.price} $</h3>
         </div>
       </div>
     </Link>
